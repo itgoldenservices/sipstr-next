c2ca15cdb2e5e19cd9b52aa817b1989c5cc28ddb
itgoldenservices@gmail.com <itgoldenservices@gmail.com>
2025-04-19 13:39:02 +0530

    h1 & p position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/ui/Header";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import Image from "next/image";
-import { Roboto } from 'next/font/google';
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 export default function Home() {
@@ -14,25 +14,27 @@ export default function Home() {
     <div className="relative bg-white min-h-screen overflow-hidden">
       <Header />
 
-      <WavyBackground className="max-w-4xl mx-auto pb-40 flex flex-col items-center">
-        <h1 className="transform -translate-y-1/2 w-xl text-2xl md:text-4xl lg:text-7xl font-bold inter-var text-center text-orange-600">
+      {/* 💬 Headline & Text OUTSIDE the wavy background */}
+      <div className="text-center mt-40 z-30 relative">
+        <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold inter-var text-orange-600">
           Coming Soon
         </h1>
-        <p className="text-base md:text-lg mt-4 text-black font-normal inter-var text-center">
+        <p className="text-base md:text-lg mt-4 text-black font-normal inter-var">
           We're working on something awesome. Stay tuned!
         </p>
-      </WavyBackground>
+      </div>
 
-      {/* Image outside the wavy div, positioned at bottom */}
+      {/* 🌊 Wavy background underneath */}
+      <WavyBackground className="max-w-4xl mx-auto pb-40 flex flex-col items-center z-10" />
+
+      {/* 🍺 Beer mug image at the bottom center */}
       <Image
         width={400}
-        height={300} // or whatever fits your image’s aspect ratio
+        height={300}
         src="/Glass.png"
         alt="Glass"
-        className="fixed top-90 bottom-0 left-1/2 transform -translate-x-1/2 z-20"
+        className="fixed bottom-0 top-100 left-1/2 transform -translate-x-1/2 z-20"
       />
-
     </div>
   );
 }
-
