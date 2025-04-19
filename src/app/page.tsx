2fa92681acf9d44e80118d7439a4520aea7edecb
itgoldenservices@gmail.com <itgoldenservices@gmail.com>
2025-04-19 13:21:33 +0530

    Sparkles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,46 @@
 import Header from "@/components/ui/Header";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import Image from "next/image";
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-});
+import { SparklesCore } from "../components/ui/sparkles";
 
 export default function Home() {
   return (
     <div className="bg-white">
-    <Header/>
+      <Header />
 
-      <WavyBackground className="max-w-4xl mx-auto pb-40">
-        <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold inter-var text-center">
+      <div className="h-[40rem] w-full  flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
           Coming Soon
         </h1>
-        <p className="text-base md:text-lg mt-4 text-black font-normal inter-var text-center">
-        We're working on something awesome. Stay tuned!
-        </p>
-        <Image
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-green-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-green-500 to-transparent h-px w-1/4" />
+
+          {/* Core component */}
+          <SparklesCore
+            background="#FFFFFF"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#EA580C"
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-white [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+      </div>
+
+      <Image
           width={200}
           height={400}
           src="/Glass.png"
           alt="Glass"
-          className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/5 w-300 z-20"
+          className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-100 z-20"
         />
-      </WavyBackground>
     </div>
   );
 }
